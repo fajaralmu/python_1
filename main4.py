e30ec1e65167aef1a66793112a2176e9ba2f47fa
Fajar AM <somabangsa@gmail.com>
2021-03-18 20:36:45 +0700

    add

---
+++ main4.py
@@ -0,0 +1,20 @@
+# Menulis ke file dengan mode append
+# file = open("hello.txt", "w")
+# file.write("HELLO")
+# file.close()
+file = open("hello.txt", "a")
+file.writelines([
+"Sekarang kita belajar menulis dengan menggunakan Python\n", 
+"Menulis konten file dengan mode a (append)\n"
+])
+file.close()
+
+file = open("hello2.txt","w")
+file.writelines(["Halo", "Belajar Python", "Menyenangkan!"])
+file.close()
+file = open("hello2.txt","w")
+file.writelines("Menulis ke dalam file")
+file.writelines("menggunakan Python")
+file = open("hello.txt","r")
+for line in file:
+    print(line)
